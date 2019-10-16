4e1a3c74d8f1f3ea4982dab28d2a527896995ead
Damix48 <damiano.zanardo@gmail.com>
2019-10-16 13:19:24 +0200

    update

--- point.cpp
+++ point.cpp
@@ -1,30 +1,29 @@
-#include <math.h>
+/* Point.h
+interfaccia di Point */
+
 #include <iostream>
 
-// Point.h
-// interfaccia di Point
 class Point {
  private:
   double x, y, z;
 
  public:
-  Point();
-  Point(double, double, double);
-  double X() const;
-  double Y() const;
-  double Z() const;
+  // Costruisco un punto di coordinate cartesiane (x, y, z)
+  Point(double _x = 0, double _y = 0, double _z = 0);
+  double getX() const;
+  double getY() const;
+  double getZ() const;
   void negate();
-  double norm();
-  Point operator+(const Point&) const;
+  double norm() const;
 };
+std::ostream& operator<<(std::ostream&, const Point&);
+Point operator+(const Point&, const Point&);
 
-// Point.cpp
-// implementazione di Point
-Point::Point() {
-  x = 0;
-  y = 0;
-  z = 0;
-}
+/* Point.cpp
+implementazione di Point */
+
+// #include "Point.h"c
+#include <math.h>
 
 Point::Point(double _x, double _y, double _z) {
   x = _x;
@@ -32,15 +31,15 @@ Point::Point(double _x, double _y, double _z) {
   z = _z;
 }
 
-double Point::X() const {
+double Point::getX() const {
   return x;
 }
 
-double Point::Y() const {
+double Point::getY() const {
   return y;
 }
 
-double Point::Z() const {
+double Point::getZ() const {
   return z;
 }
 
@@ -50,17 +49,23 @@ void Point::negate() {
   z = -z;
 }
 
-double Point::norm() {
+double Point::norm() const {
   return sqrt(x * x + y * y + z * z);
 }
 
-Point Point::operator+(const Point& p) const {
-  return Point(x + p.x, y + p.y, z + p.z);
+Point operator+(const Point& p1, const Point& p2) {
+  return Point(p1.getX() + p2.getX(), p1.getY() + p2.getY(),
+               p1.getZ() + p2.getZ());
 }
 
 std::ostream& operator<<(std::ostream& os, const Point& p) {
-  return os << "(" << p.X() << ", " << p.Y() << ", " << p.Z() << ")";
+  return os << "(" << p.getX() << ", " << p.getY() << ", " << p.getZ() << ")";
 }
+
+// main.cpp
+
+// #include "Point.h"
+#include <iostream>
 
 int main() {
   Point A(1, 2, 3);
